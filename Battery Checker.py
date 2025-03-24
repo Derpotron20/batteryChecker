763a48d21a60081cdbc842b97ce8848a57cffbc9
Derpotron20 <matthew.f.a.braga@gmail.com>
2025-03-24 20:28:59 +1100

    Wait & Reset
    Now if will notify you, then wait 30 minutes. at any point if the status of your computer changes from on state to another (under 20 to between to over 80), it resets the timer. After the 30 minutes, will send another notification if required, but new notification will only last 5 seconds so as to not be annoying. Will then repeat that over and over again until status changes.

--- Battery Checker.py
+++ Battery Checker.py
@@ -2,36 +2,61 @@ import psutil
 from plyer import notification
 import time
 
-responded = False
+notified = False
 timer = 0
-timeout = False
+timeouter = False
 
 while True:
-    battery = psutil.sensors_battery()
-    if not timeout:
-        waittime = 10^1000
+    if timeouter:
+        wait = 5
     else:
-        waittime = 5
+         wait = 10^1000
+
+
+    battery = psutil.sensors_battery()
+
+
+    if notified:
+        if battery.power_plugged and battery.percent > 80:
+            new_status = 2
+        elif not battery.power_plugged and battery.percent < 20:
+            new_status = 0
+        else:
+            new_status = 1
+
+        if new_status != status:
+            notified = False
+            timer = 0
+            timeouter = False
+            new_status = None
+            status = None
+
+
     if battery.power_plugged and battery.percent > 80:
-        if not responded:
+        if not notified:
             notification.notify(
                 title = 'Battery Percentage',
                 message = 'Over 80, recommend unplugging',
                 app_icon = 'Battery Symbol.ico',
-                timeout = waittime,
+                timeout = wait,
             )
-            responded = True
+            notified = True
+            status = 2
     elif not battery.power_plugged and battery.percent < 20:
-        if not responded:
+        if not notified:
             notification.notify(
                 title = 'Battery Percentage',
                 message = 'Under 20, recommend plugging in',
                 app_icon = 'Battery Symbol.ico',
-                timeout = waittime,
+                timeout = wait,
             )
-            responded = True
+            notified = True
+            status = 0
+
+
     time.sleep(60)
     timer += 1
     if timer == 30:
-        responded = False
-        timeout = True
+        notified = False
+        timeouter = True
+        timer = 0
