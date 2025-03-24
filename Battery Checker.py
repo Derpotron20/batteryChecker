0fbc3ad9e9aa090c84c1b962846b5abf3adb1c7a
Derpotron20 <matthew.f.a.braga@gmail.com>
2025-03-24 18:32:33 +1100

    Responsive
    If you respond to the notification (required for it to leave), even if you don't do it, it will then wait 30 minutes before pinging you again, then this time it will wait 5 seconds, then auto go away, to decrease annoyance if you want to get rid of it.

--- Battery Checker.py
+++ Battery Checker.py
@@ -2,20 +2,36 @@ import psutil
 from plyer import notification
 import time
 
+responded = False
+timer = 0
+timeout = False
+
 while True:
     battery = psutil.sensors_battery()
+    if not timeout:
+        waittime = 10^1000
+    else:
+        waittime = 5
     if battery.power_plugged and battery.percent > 80:
-        notification.notify(
-            title = 'Battery Percentage',
-            message = 'Over 80, recommend unplugging',
-            app_icon = 'Battery Symbol.ico',
-            timeout = 10*10^1000,
-        )
+        if not responded:
+            notification.notify(
+                title = 'Battery Percentage',
+                message = 'Over 80, recommend unplugging',
+                app_icon = 'Battery Symbol.ico',
+                timeout = waittime,
+            )
+            responded = True
     elif not battery.power_plugged and battery.percent < 20:
-        notification.notify(
-            title = 'Battery Percentage',
-            message = 'Under 20, recommend plugging in',
-            app_icon = 'Battery Symbol.ico',
-            timeout = 10*10^1000,
-        )
+        if not responded:
+            notification.notify(
+                title = 'Battery Percentage',
+                message = 'Under 20, recommend plugging in',
+                app_icon = 'Battery Symbol.ico',
+                timeout = waittime,
+            )
+            responded = True
     time.sleep(60)
+    timer += 1
+    if timer == 30:
+        responded = False
+        timeout = True
